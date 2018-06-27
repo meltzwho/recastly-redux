5f63e92193d071e7e8c4655731309e6f2375a263
Eric Meltzer <meltzer.eric@live.com>
2018-06-27 19:05:03 -0400

    End of day

--- src/actions/search.js
+++ src/actions/search.js
@@ -2,11 +2,18 @@ import searchYouTube from '../lib/searchYouTube.js';
 import changeVideoList from './videoList.js';
 import changeVideo from './currentVideo.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
-
+import thunk from '../../node_modules/redux-thunk';
 
 var handleVideoSearch = (q) => {
 
   //TODO:  Write an asynchronous action to handle a video search!
+  return function thunk(dispatch){
+    return searchYouTube({query: q, key: YOUTUBE_API_KEY}, (data) => {
+      dispatch(changeVideo(data[0]));
+      dispatch(changeVideoList(data));
+
+    });
+  };
 };
 
 export default handleVideoSearch;
